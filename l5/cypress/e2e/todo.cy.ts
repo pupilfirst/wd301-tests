713543a0c959fb9cd9886005e2a8c0dc4179a7e6
Vignesh Rajendran <vign.ran@gmail.com>
2023-05-17 19:25:20 +0530

    Updated test descriptions in L5 (#6)
    Co-authored-by: Hari Gopal <mail@harigopal.in>

--- l5/cypress/e2e/todo.cy.ts
+++ l5/cypress/e2e/todo.cy.ts
@@ -6,16 +6,6 @@ const todoItems = [
     description: "item 1 description",
     dueDate: "2023-01-09",
   },
-  {
-    title: "Sample item 2",
-    description: "item 2 description",
-    dueDate: "2023-01-09",
-  },
-  {
-    title: "Sample item 3",
-    description: "item 3 description",
-    dueDate: "2023-01-09",
-  },
 ];
 
 const addEntries = () => {
@@ -32,7 +22,7 @@ if (studentSubmissionUrl.endsWith("/")) {
   studentSubmissionUrl = studentSubmissionUrl.slice(0, -1);
 }
 
-describe("After signing in,", () => {
+describe("After signing in with the username `admin`, and password `admin`,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -97,16 +87,9 @@ describe("After signing in and visiting the `/tasks` page,", () => {
     cy.reload();
     cy.get(".TaskItem").should("have.length", todoItems.length);
   });
-
-  it("deleting a few added items should reduce the number of `.TaskItem` elements on the page", () => {
-    addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".TaskItem").should("have.length", todoItems.length - 2);
-  });
 });
 
-describe("Level 5 milestone task details Page", () => {
+describe("After signing in and visiting the `/tasks` page,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -116,62 +99,33 @@ describe("Level 5 milestone task details Page", () => {
     cy.get("button[type='submit']").click();
     cy.visit(studentSubmissionUrl + "/tasks");
   });
-  it("should click and lead to task details page", () => {
+
+  it("clicking on the task title should navigate to task details page", () => {
     addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
     cy.get(".text-base").click();
     cy.get("h3").should("be.visible");
   });
-  it("should click on item 1 and lead to task details page", () => {
+
+  it("clicking on the task title should navigate to task details page with the same title rendered in a `<h3>` heading element", () => {
     addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".text-base").should("have.text", "Sample item 1");
-  });
-  it("should have the right task details", () => {
-    addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
     cy.get(".text-base").click();
     cy.get("h3").should("have.text", "Sample item 1");
   });
 });
 
-describe("Level 5 milestone should validate protected route", () => {
-  it("while visiting Homepage and redirect to sign-in page when unauthenticated", () => {
+describe("With protected routes implemented and while the user is not signed in,", () => {
+  it("visiting the homepage should redirect back to the `/signin` page", () => {
     cy.visit(studentSubmissionUrl);
-    cy.location("pathname").should("equal", "/signin"); // Verify that we are redirected to the sign-in page
+    cy.location("pathname").should("equal", "/signin");
   });
 
-  it("while visiting Tasks page and redirect to sign-in page when unauthenticated", () => {
+  it("visiting the `/tasks` page should redirect back to the `/signin` page", () => {
     cy.visit(studentSubmissionUrl + "/tasks");
-    cy.location("pathname").should("equal", "/signin"); // Verify that we are redirected to the sign-in page
-  });
-});
-
-describe("Level 5 milestone should validate protected route after signin", () => {
-  beforeEach(() => {
-    cy.visit(studentSubmissionUrl);
-    cy.get("#username").clear();
-    cy.get("#username").type("admin");
-    cy.get("#password").clear();
-    cy.get("#password").type("admin");
-    cy.get("button[type='submit']").click();
-  });
-
-  it("and redirect to Home page when authenticated", () => {
-    cy.get("nav").should("be.visible");
-  });
-
-  it("and redirect to Signin page when clicked on Signout", () => {
-    cy.visit(studentSubmissionUrl + "/signin");
-    cy.visit(studentSubmissionUrl);
     cy.location("pathname").should("equal", "/signin");
   });
 });
 
-describe("Level 5 milestone should validate undefined route", () => {
+describe("With protected routes implemented and the user signed in,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -181,13 +135,34 @@ describe("Level 5 milestone should validate undefined route", () => {
     cy.get("button[type='submit']").click();
   });
 
-  it("and redirect to NotFound page on invalid path", () => {
+  it("the application should redirect to the homepage and the `<nav>` element should be visible", () => {
+    cy.get("nav").should("be.visible");
+  });
+});
+
+describe("With protected routes implemented and while the user signs in with the username `admin` and password `admin`,", () => {
+  beforeEach(() => {
+    cy.visit(studentSubmissionUrl);
+    cy.get("#username").clear();
+    cy.get("#username").type("admin");
+    cy.get("#password").clear();
+    cy.get("#password").type("admin");
+    cy.get("button[type='submit']").click();
+  });
+
+  it("the application redirects to the `/notfound` page on visting an invalid path", () => {
     cy.visit(studentSubmissionUrl + "/test");
     cy.location("pathname").should("equal", "/notfound");
   });
 
-  it("and hide the Header in NotFound page", () => {
+  it("the application does not contain the `<nav>` element on the `/notfound` page", () => {
     cy.visit(studentSubmissionUrl + "/test");
     cy.get("nav").should("have.length", 0);
+  });
+
+  it("the application redirects back to the Home page from the `/notfound` page while clicking on the `#backToHomeButton`", () => {
+    cy.visit(studentSubmissionUrl + "/test");
+    cy.get("#backToHomeButton").click();
+    cy.location("pathname").should("equal", "/");
   });
 });
