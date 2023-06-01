11095d868b1e1003f790f4e08037b6e63cddf43e
Hari Gopal <mail@harigopal.in>
2023-06-01 14:40:08 +0530

    Use import syntax instead of require

--- l6/cypress.config.ts
+++ l6/cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
+import cypressJsonResults from "cypress-json-results";
 
 export default defineConfig({
   e2e: {
     experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require("cypress-json-results")({
+      cypressJsonResults({
         on,
         filename: "results.json",
       });
