452583be5566e81f5d05eeab03ad68bc4322b53b
Hari Gopal <mail@harigopal.in>
2023-07-25 23:48:47 +0530

    Improve L7 test descriptions
    There's still an issue with the delete button click test; it's a loose
    test that's clicking _some_ button within a `.member` element.

--- l7/cypress/e2e/member-module.cy.ts
+++ l7/cypress/e2e/member-module.cy.ts
@@ -1,11 +1,23 @@
-let studentSubmissionUrl = Cypress.env("STUDENT_SUBMISSION_URL") || "http://localhost:3000";
+let studentSubmissionUrl =
+  Cypress.env("STUDENT_SUBMISSION_URL") || "http://localhost:3000";
+
 if (studentSubmissionUrl.endsWith("/")) {
   studentSubmissionUrl = studentSubmissionUrl.slice(0, -1);
 }
 
-describe("Preparing for Level 7 milestone testing, first we will signup", () => {
-  it("should visit signup path and create an account", () => {
+const signIn = () => {
+  cy.visit(studentSubmissionUrl + "/signin");
+  cy.get("#email").clear();
+  cy.get("#email").type("alice@acme.com");
+  cy.get("#password").clear();
+  cy.get("#password").type("12345678");
+  cy.get("button[type='submit']").click();
+};
+
+describe("When signed out,", () => {
+  it("visiting the `/signup` path should allow users to sign up by filling in `#organisationName`, `#userName`, `#userEmail`, `#userPassword`, and clicking the submit button", () => {
     cy.visit(studentSubmissionUrl + "/signup");
+
     cy.get("#organisationName").clear();
     cy.get("#organisationName").type("ACME Corp");
     cy.get("#userName").clear();
@@ -16,32 +28,29 @@ describe("Preparing for Level 7 milestone testing, first we will signup", () => 
     cy.get("#userPassword").type("12345678");
     cy.get("button[type='submit']").click();
   });
+
+  it("visiting `/signin` and filling in `#email`, `#password` and clicking the submit button, the user should be redirected to `/account/projects`", () => {
+    signIn();
+
+    cy.location("pathname").should("equal", "/account/projects");
+  });
 });
 
+describe("After signing in", () => {
+  beforeEach(signIn);
 
-describe("After signing in, ", () => {
-  beforeEach(() => {
-    cy.visit(studentSubmissionUrl + "/signin");
-    cy.get("#email").clear();
-    cy.get("#email").type("alice@acme.com");
-    cy.get("#password").clear();
-    cy.get("#password").type("12345678");
-    cy.get("button[type='submit']").click();
+  it("visiting the `/account/members` page should show the signed in user's email address and name", () => {
+    cy.visit(studentSubmissionUrl + "/account/members");
+    cy.contains("alice@acme.com", { matchCase: false });
+    cy.contains("Alice", { matchCase: false });
   });
 
-  it("should visit members page and find a member alice", () => {
-    cy.location("pathname").should("equal", "/account/projects");
+  it("visiting `/account/members` and clicking `#new-member-btn` should allow users to fill in `#name`, `#email`, `#password`, and clicking `#create-member-btn` should add the new user's email address and name in the list of members", () => {
     cy.visit(studentSubmissionUrl + "/account/members");
-    cy.contains("alice@acme.com", { matchCase: false })
-    cy.contains("Alice", { matchCase: false })
-  })
 
-  it("should visit members page and create a new member", () => {
-    cy.location("pathname").should("equal", "/account/projects");
-    cy.visit(studentSubmissionUrl + "/account/members");
     cy.get("#new-member-btn").click();
 
-    let x = Math.floor((Math.random() * 10000) + 1);
+    let x = Math.floor(Math.random() * 10000 + 1);
 
     cy.get("#name").clear();
     cy.get("#name").type(`Alice ${x}`);
@@ -51,19 +60,18 @@ describe("After signing in, ", () => {
     cy.get("#password").type("12345678");
     cy.get("#create-member-btn").click();
 
-    cy.contains(`alice${x}@yopmail.com`, { matchCase: false })
-    cy.contains(`Alice ${x}`, { matchCase: false })
-  })
+    cy.contains(`alice${x}@yopmail.com`, { matchCase: false });
+    cy.contains(`Alice ${x}`, { matchCase: false });
+  });
 
-  it("should visit members page and delete the newly created member", () => {
-    cy.location("pathname").should("equal", "/account/projects");
+  it("visiting the `/account/members` page should show a delete button that can be clicked", () => {
     cy.visit(studentSubmissionUrl + "/account/members");
 
-    cy.get('.member')  // Select all cards with classname ".member"
-    .last()          // Select the last card
-    .within(() => {
-      cy.get('button')  // Select the button within the last card
-        .click();      // Perform an action on the button (e.g., click)
-    });    
-  })
+    cy.get(".member") // Select all cards with classname ".member"
+      .last() // Select the last card
+      .within(() => {
+        cy.get("button") // Select the button within the last card
+          .click(); // Perform an action on the button (e.g., click)
+      });
+  });
 });
