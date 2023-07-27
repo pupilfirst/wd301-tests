348d15868d54e2a7067b07131ae623a9c367613b
Hari Gopal <mail@harigopal.in>
2023-07-27 18:53:29 +0530

    Update L3 tests for Vite

--- l3/cypress/e2e/todo.cy.ts
+++ l3/cypress/e2e/todo.cy.ts
@@ -1,62 +1,84 @@
 /// <reference types="cypress" />
-describe('', () => {
+
+let studentSubmissionUrl =
+  Cypress.env("STUDENT_SUBMISSION_URL") || "http://localhost:5173";
+
+if (studentSubmissionUrl.endsWith("/")) {
+  studentSubmissionUrl = studentSubmissionUrl.slice(0, -1);
+}
+
+describe("", () => {
   beforeEach(() => {
-    cy.visit(Cypress.env("STUDENT_SUBMISSION_URL"))
-  })
+    cy.visit(studentSubmissionUrl);
+  });
 
-  it('The submit button should have `addTaskButton` as its `id`', () => {
-    cy.get('#addTaskButton').should('exist');
-  })
+  it("The submit button should have `addTaskButton` as its `id`", () => {
+    cy.get("#addTaskButton").should("exist");
+  });
 
-  it('The title field should have `todoTitle` as its `id`', () => {
-    cy.get('#todoTitle').should('exist');
-  })
+  it("The title field should have `todoTitle` as its `id`", () => {
+    cy.get("#todoTitle").should("exist");
+  });
 
-  it('The description field should have `todoDescription` as its `id`', () => {
-    cy.get('#todoDescription').should('exist');
-  })
+  it("The description field should have `todoDescription` as its `id`", () => {
+    cy.get("#todoDescription").should("exist");
+  });
 
-  it('The due date field should have `todoDueDate` as its `id`', () => {
-    cy.get('#todoDueDate').should('exist');
-  })
+  it("The due date field should have `todoDueDate` as its `id`", () => {
+    cy.get("#todoDueDate").should("exist");
+  });
 
-  it('Adding some sample items should result in an equal number of .TaskItem elements', () => {
+  it("Adding some sample items should result in an equal number of .TaskItem elements", () => {
     const todoItems = [
-      {title: "Sample item 1", description: "item 1 description", dueDate: "2023-01-09"},
-      {title: "Sample item 2", description: "item 2 description", dueDate: "2023-01-09"},
-      {title: "Sample item 3", description: "item 3 description", dueDate: "2023-01-09"},
-    ]
+      {
+        title: "Sample item 1",
+        description: "item 1 description",
+        dueDate: "2023-01-09",
+      },
+      {
+        title: "Sample item 2",
+        description: "item 2 description",
+        dueDate: "2023-01-09",
+      },
+      {
+        title: "Sample item 3",
+        description: "item 3 description",
+        dueDate: "2023-01-09",
+      },
+    ];
 
-    todoItems.forEach(item => {
-      cy.get('#todoDueDate').type(`${item.dueDate}`)
-      cy.get('#todoTitle').type(`${item.title}`)
-      cy.get('#todoDescription').type(`${item.description}`)
-      cy.get('#addTaskButton').click();
-    })
+    todoItems.forEach((item) => {
+      cy.get("#todoDueDate").type(`${item.dueDate}`);
+      cy.get("#todoTitle").type(`${item.title}`);
+      cy.get("#todoDescription").type(`${item.description}`);
+      cy.get("#addTaskButton").click();
+    });
 
-    cy.get('.TaskItem')
-      .should('have.length', todoItems.length)
-  })
+    cy.get(".TaskItem").should("have.length", todoItems.length);
+  });
 
-  it('It should not be possible to add an item without a title or a due date', () => {
+  it("It should not be possible to add an item without a title or a due date", () => {
     const todoItems = [
-      {title: "Sample item 1", description: "item 1 description", dueDate: "2023-01-09"},
-    ]
+      {
+        title: "Sample item 1",
+        description: "item 1 description",
+        dueDate: "2023-01-09",
+      },
+    ];
 
-    todoItems.forEach(item => {
-      cy.get('#todoTitle').type(`${item.title}`)
-      cy.get('#addTaskButton').click();
-    })
+    todoItems.forEach((item) => {
+      cy.get("#todoTitle").type(`${item.title}`);
+      cy.get("#addTaskButton").click();
+    });
 
-    cy.get('#todoTitle').clear()
+    cy.get("#todoTitle").clear();
 
-    todoItems.forEach(item => {
-      cy.get('#todoDueDate').type(`${item.dueDate}`)
-      cy.get('#todoDescription').type(`${item.description}`)
-      cy.get('#addTaskButton').click();
-    })
+    todoItems.forEach((item) => {
+      cy.get("#todoDueDate").type(`${item.dueDate}`);
+      cy.get("#todoDescription").type(`${item.description}`);
+      cy.get("#addTaskButton").click();
+    });
 
-    cy.get('.TaskItem')
-      .should('have.length', 0)
-  })
-})
+    cy.get(".TaskItem").should("have.length", 0);
+  });
+});
--- l3/src/types.ts
+++
@@ -1,6 +0,0 @@
-
-export interface TaskItem {
-  title: string;
-  description: string;
-  dueDate: string;
-}
