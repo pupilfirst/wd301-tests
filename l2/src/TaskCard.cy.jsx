2012fa17d6e93174e74e4dba7b7cb8f60c9d3b42
Hari Gopal <mail@harigopal.in>
2023-07-31 16:31:36 +0530

    Improve L2 test descriptions

--- l2/src/TaskCard.cy.jsx
+++ l2/src/TaskCard.cy.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import TaskCard from './TaskCard'
 
-describe('<TaskCard /> component', () => {
-  it('when given an `assigneeName` prop, renders it', () => {
+describe('<TaskCard /> component, ', () => {
+  it('when given an `assigneeName` prop, renders the value', () => {
     cy.mount(<TaskCard assigneeName="Avishek"/>)
     cy.contains("Avishek")
   })
 
-  it('when given a `dueDate` renders it as due, and not completed', () => {
+  it('when given a `dueDate` prop, renders it as `Due on: DATE`', () => {
     cy.mount(<TaskCard dueDate="22nd March" />)
     cy.contains("Due on: 22nd March", { matchCase: false })
     cy.contains("completed on: 22nd March", { matchCase: false }).should('not.exist')
   })
 
-  it('when given a `completedAtDate`, renders completed on date and not as due', () => {
+  it('when given a `completedAtDate` prop, renders it as `Completed on: DATE`', () => {
     cy.mount(<TaskCard completedAtDate="22nd March" />)
     cy.contains("Completed on: 22nd March", { matchCase: false })
     cy.contains("Due on: 22nd March", { matchCase: false }).should('not.exist')
   })
 
-  it('when given `title`, `completedAtDate` and `assigneeName`, renders all the details', () => {
+  it('when given `title`, `completedAtDate` and `assigneeName` props, renders the title as-is, the assignee as `Assignee: NAME`, and completed date as `Completed on: DATE`', () => {
     const props = {
       title: 'Finish React course',
       completedAtDate: '12th April',
@@ -30,9 +30,9 @@ describe('<TaskCard /> component', () => {
     cy.contains(props.title)
     cy.contains(`Completed on: ${props.completedAtDate}`)
     cy.contains(`Assignee: ${props.assigneeName}`)
-  })
+  });
 
-  it('when given `title`, `dueDate` and `assigneeName`, renders all the details', () => {
+  it('when given `title`, `dueDate` and `assigneeName`, renders the title as-is, the assignee as `Assignee: NAME` and due date as `Due on: DATE`', () => {
     const props = {
       title: 'Finish React course',
       dueDate: '20th April',
